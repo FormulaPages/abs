ef88bc856090225e8542889df45cc09203828600
Peter Moresi <peter.moresi@gmail.com>
2015-10-06 21:31:48 -0700

    v1.0.1 - Add test for non-numeric values

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 var abs = require('./ABS');
 var assert = require('assert');
+var error = require('formula-errors');
 
 describe('abs', function() {
   it('should convert number to absolute value', function() {
     assert( abs(1) === 1, 'Should still be 1');
     assert( abs(-1) === 1, 'Should still be 1');
+    assert( abs('foo') === error.value, 'Should still be error.value');
+    assert( abs(NaN) === error.value, 'Should still be error.value');
+    assert( abs(Number.POSITIVE_INFINITY) === error.value, 'Should still be error.value');
+    assert( abs(Number.NEGATIVE_INFINITY) === error.value, 'Should still be error.value');
   });
 })
